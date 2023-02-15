11c46e3669181c356c4e5755d6633f895e8bec47
Noah-OConnor <97732490+Nocleus@users.noreply.github.com>
2023-02-15 15:52:01 -0500

    Look At NPC

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public LayerMask npc;
+    public GameObject camPov;
     private bool inRange;
+    private Coroutine LookCoroutine;
+    public float rotationSpeed = 1f;
+    
     private void Update()
     {
         // checks if NPC is in range
@@ -19,5 +23,42 @@ public class Player : MonoBehaviour
         {
             GameManager.Instance.npcInRange = false;
         }
+
+        if ( GameManager.Instance.fDown)
+        {
+            StartRotating();
+        }
+    }
+
+    public void StartRotating()
+    {
+        if (LookCoroutine != null)
+        {
+            StopCoroutine(LookCoroutine);
+        }
+
+        LookCoroutine = StartCoroutine(LookAt());
+    }
+
+    private IEnumerator LookAt()
+    {
+        camPov.GetComponent<PlayerLook>().enabled = false;
+        Transform Target = GameManager.Instance.frog;
+        Quaternion lookRotation = Quaternion.LookRotation(Target.position - transform.position);
+        Quaternion camLookRotation = Quaternion.LookRotation(Target.position - camPov.transform.position);
+
+        float time = 0;
+        while (time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, time);
+            transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f);
+
+            camPov.transform.rotation = Quaternion.Slerp(camPov.transform.rotation, camLookRotation, time);
+            //camPov.transform.eulerAngles = new Vector3(camPov.transform.eulerAngles.x, 0f, 0f);
+            
+            time += Time.deltaTime * rotationSpeed;
+            yield return null;
+        }
+        //camPov.GetComponent<PlayerLook>().enabled = true;
     }
 }
