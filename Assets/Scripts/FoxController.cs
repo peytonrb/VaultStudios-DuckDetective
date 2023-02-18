418de63148e779e8d4bccbb6784751a7026e514c
Peyton Bischof <peyton.bischof@hotmail.com>
2023-02-18 12:01:31 -0500

    AI

--- Assets/Scripts/FoxController.cs
+++ Assets/Scripts/FoxController.cs
@@ -7,20 +7,37 @@ public class FoxController : MonoBehaviour
 {
     public float dangerRadius;
     public Transform player;
+    public Transform[] patrolPoints;
     public LayerMask isPlayer;
     public float speed;
     private bool withinRadius;
     private NavMeshAgent agent;
+    private int destinationPoint;
 
     void Start() {
         agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        nextPoint();
     }
     void Update()
     {
         withinRadius = Physics.CheckSphere(transform.position, dangerRadius, isPlayer);
 
+        if (!agent.pathPending && agent.remainingDistance < 0.5f) {
+            nextPoint();
+        }
+
         if (withinRadius) {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
+    }
+
+    void nextPoint() {
+        if (patrolPoints.Length == 0) {
+            return;
+        }
+
+        agent.destination = patrolPoints[destinationPoint].position;
+        destinationPoint = (destinationPoint + 1) % patrolPoints.Length;
     }
 }
