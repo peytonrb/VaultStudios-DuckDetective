7f5ad7c74163b091c3a2f772ea6a07c1e057ad54
Noah-OConnor <97732490+Nocleus@users.noreply.github.com>
2023-02-17 21:10:41 -0500

    MoreNPCWork/ArtScene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -51,5 +52,10 @@ public class GameManager : MonoBehaviour
         {
             fDown = false;
         }
+    }
+
+    public void WinGame()
+    {
+        Debug.Log("WinGame");
     }
 }
--- Assets/Scripts/npcChat.cs
+++ Assets/Scripts/npcChat.cs
@@ -6,15 +6,14 @@ using TMPro;
 public class npcChat : MonoBehaviour
 {
     public TMP_Text[] chats;
+    public TMP_Text winChat;
     public int chatNumb;
     private bool pickup;
+    private bool spokenTo;
 
     private void Awake()
     {
-        foreach (TMP_Text text in chats)
-        {
-            text.gameObject.SetActive(false);
-        }
+        deactivateStuff();
         chatNumb = 0;
         pickup = false;
     }
@@ -26,8 +25,15 @@ public class npcChat : MonoBehaviour
             if (GameManager.Instance.moveOff && !GameManager.Instance.activeChat)
             {
                 GameManager.Instance.activeChat = true;
-                chats[chatNumb].gameObject.SetActive(true);
-                //Debug.Log(chats.Length);
+                if (GameManager.Instance.hasMacguffin && tag != "Macguffin")
+                {
+                    winChat.gameObject.SetActive(true);
+                }
+                else
+                {
+                    chats[chatNumb].gameObject.SetActive(true);
+                }
+                
                 if ((chatNumb + 1) >= chats.Length && tag != "Macguffin")
                 {
                     return;
@@ -44,13 +50,7 @@ public class npcChat : MonoBehaviour
             }
             else if (!GameManager.Instance.activeChat)
             {
-                foreach (TMP_Text text in chats)
-                {
-                    if (text.gameObject.activeInHierarchy)
-                    {
-                        text.gameObject.SetActive(false);
-                    }
-                }
+                deactivateStuff();
             }
         }
 
@@ -73,6 +73,21 @@ public class npcChat : MonoBehaviour
             {
                 text.gameObject.SetActive(false);
             }
+        }
+    }
+
+    private void deactivateStuff()
+    {
+        foreach (TMP_Text text in chats)
+        {
+            if (text.gameObject.activeInHierarchy)
+            {
+                text.gameObject.SetActive(false);
+            }
+        }
+        if (winChat != null)
+        {
+            winChat.gameObject.SetActive(false);
         }
     }
 }
