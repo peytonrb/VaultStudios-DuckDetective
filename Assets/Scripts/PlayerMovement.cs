84dc012909876360e85d6b59bdd1110eaf59cf6c
Peyton Bischof <peyton.bischof@hotmail.com>
2023-02-24 21:22:32 -0500

    Critical Path

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller; 
-    public float speed = 12f;
+    public float speed;
     public float gravity = -9.8f;
     public float jumpHeight = 3f;
     public Transform groundCheck;
