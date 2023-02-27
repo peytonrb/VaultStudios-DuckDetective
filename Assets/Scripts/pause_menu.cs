d6c0a3ad1dac533c27f8a729a935581fcf9b2bd3
a-rt-j <adam.j.rodney@knights.ucf.edu>
2023-02-27 01:20:32 -0500

    Menu fixes, P to Pause
    Loss menu still doesn't work

--- Assets/Scripts/pause_menu.cs
+++ Assets/Scripts/pause_menu.cs
@@ -13,7 +13,7 @@ public class pause_menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
         {
 
 
